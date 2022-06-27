7b86cd9f5ed5de36cc28797cfa86c6fb0beb9a0a
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-06-27 22:30:53 +0200

    fix: part1

--- createusers.sh
+++ createusers.sh
@@ -10,20 +10,20 @@ function checkGroups () {
         echo "No groups specified"
         exit 1
     fi
-    
+
     local user=$1
     local groups=$2
     grps=($(echo -e "$groups" | awk -F: '{$NF=$1=$2=$3=""; sub(/^[ \t]+/, ""); print $0}'))
     for grp in "${grps[@]}"; do
-        if [ -z "$(grep ${grp} /etc/group)" ]; then
+        if [ -z "$(grep -e ^${grp} /etc/group)" ]; then
             echo "Group ${grp} does not exist"
-            groupadd ${grp}
-            echo "Group ${grp} created"
+            groupadd ${grp} && echo "Group ${grp} created" || echo "Failed to create group ${grp}"
         fi
 
-        usermod -a -G ${grp} ${LOGIN} && echo "User ${LOGIN} added to group ${grp}" || echo "User ${LOGIN} not added to group ${grp}"
+        usermod -aG ${grp} ${LOGIN} && echo "User ${LOGIN} added to group ${grp}" || echo "User ${LOGIN} not added to group ${grp}"
         
     done
+
 }
 
 
@@ -31,7 +31,7 @@ if [ -z "$(grep groupe1 /etc/group)" ]; then
     groupadd groupe1
 fi
 
-awk -F: '{print $0}' users.txt | while read line; do
+cat users.txt | while read line; do
   LOGIN=$(echo $line | awk -F: '{print $1}')
   PASSWORD=$(echo $line | awk -F: '{print $NF}')
 
@@ -41,8 +41,23 @@ awk -F: '{print $0}' users.txt | while read line; do
 
     # Create the user
     useradd $LOGIN --create-home --groups groupe1 --password $PASSWORD
+    chage --lastday 0 $LOGIN
+    nb=$((5 + RANDOM % $((10-5))))
     echo "User $LOGIN created with password $PASSWORD"
+    for ((i=0;i<nb;i++)); do
+      size=$((5 + RANDOM % $((50-5))))
+      filename="ratio$i"
+      path="/home/$LOGIN/$filename"
+      fallocate -l $size\MiB $filename
+      echo "File $filename created"
+      echo "move to $path"
+      mv $filename $path
+      chown $LOGIN:groupe1 $path
+    done
+
   fi
 
   checkGroups $LOGIN $line
 done
+
+echo "Done"
